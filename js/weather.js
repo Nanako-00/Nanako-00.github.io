5ffe1b01b04e7f841b89d63d956b8ac89c6c2eaa
Nanako <mengfei_liu@dnt.com.cn>
2023-06-07 21:04:24 +0800

    Site updated: 2023-06-07 21:04:19

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "0124",
+    "background": "5",
+    "tmpColor": "F7BB60",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "cf710d0f813e4260a2bf9cf4c41d839f"
+  }
+}
